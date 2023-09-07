50a005772270cce964bb3ef046e0e56cfd4234ce
Black-ceaser <calvin.annan@gmail.com>
2023-09-07 08:39:15 -0700

    Programming is like building a multilingual puzzle

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ *main . A program that prints a line with puts function and not printf
+ *Return:(Succes)
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
