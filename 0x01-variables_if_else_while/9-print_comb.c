c1df29286136acdd4a61326df73fcec627058cef
Black-ceaser <calvin.annan@gmail.com>
2023-09-08 18:58:56 -0700

    combination of numbers

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - Patience, persistence and perspiration
+ *  make an unbeatable combination for success
+ */
+
+int main(void)
+{
+	int digit;
+
+	for (digit = 0; digit <= 9; digit++)
+	{
+		putchar(digit + '0');
+		if (digit < 9)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+	return (0);
+}
