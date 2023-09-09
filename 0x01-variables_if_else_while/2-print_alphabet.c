8e42ad57376eec6aa928405c88e5078ec7ca21d2
Black-ceaser <calvin.annan@gmail.com>
2023-09-08 17:21:47 -0700

    printing lower case letters

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - A program that prints the lower case alphabets
+ * Return: (0)Success
+ */
+
+int main(void)
+{
+	char c;
+
+	c = 'a';
+	while (c <= 'z')
+	{
+		putchar(c);
+		c++;
+	}
+	return (0);
+}
