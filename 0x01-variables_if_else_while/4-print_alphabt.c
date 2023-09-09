80fd6c2000ac0d793a1d64e0a3dd3135db0bc535
Black-ceaser <calvin.annan@gmail.com>
2023-09-08 17:56:00 -0700

    excluding letters

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - Excluding e and q
+ * Return: (0)Success
+ */
+
+int main(void)
+{
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++)
+	{
+		if (c != 'e' && c != 'q')
+		{
+			putchar(c);
+		}
+	}
+	putchar('\n');
+	return (0);
+}
