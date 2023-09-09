ebb5634d3d2b8006cadfbb20fa6fbb7fd1204e67
Black-ceaser <calvin.annan@gmail.com>
2023-09-08 17:44:57 -0700

    both lower and upper case

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - A program that prints lowercase and uppercase letters
+ * Return: (0) Success
+ */
+
+int main(void)
+{
+	char c;
+
+	char d;
+
+	c = 'a';
+	d = 'A';
+	while (c <= 'z')
+	{
+		putchar(c);
+		c++;
+	}
+	while (d <= 'Z')
+	{
+		putchar(d);
+		d++;
+	}
+	putchar('\n');
+	return (0);
+}
