cb055d0c41f9df24268bc6829deac8e0524125c3
Black-ceaser <calvin.annan@gmail.com>
2023-09-11 06:58:56 -0700

    printing to the standard error

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - Printing to the standard error (stderr)
+ * Return: (0):Success
+ */
+
+int main(void)
+{
+	fprintf(stderr, "and that piece of art is useful"
+			"- Dora Korpar, 2015-10-19\n");
+	return (1);
+}
