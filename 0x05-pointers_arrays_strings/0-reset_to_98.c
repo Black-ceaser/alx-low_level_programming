f59fcb685e0c3e5c1a774d7a7b872694cbef52e7
Black-ceaser <calvin.annan@gmail.com>
2023-09-18 08:24:22 -0700

    98 Battery st.

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - Updates the values of a given parameter
+ * @n: the value to be changed
+ * Return: (void)success
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
