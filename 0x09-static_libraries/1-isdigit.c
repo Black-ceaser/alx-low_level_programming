a355f8b015f07c0318166fe87e1d8e6c0195cdc6
Black-ceaser <calvin.annan@gmail.com>
2023-09-29 13:44:00 -0700

    My first library in c

---
+++ 0x09-static_libraries/1-isdigit.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _isdigit - checks through 0 t0 9
+ * @c: the variable to be checked
+ *Return: 0 or 1
+ */
+
+int _isdigit(int c)
+{
+	if (c >= '0' && c <= '9')
+		return (1);
+	else
+		return (0);
+}
