8d3503639311ca2053e6b0a4b7d6e00179bb1cc2
Black-ceaser <calvin.annan@gmail.com>
2023-10-20 10:34:29 -0700

    printing the elments in a linked list

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stddef.h>
+#include "lists.h"
+/**
+ * print_list - prints elements in a list
+ * *h: pointer for list_t
+ * Return: number of nodes
+ */
+size_t print_list(const list_t *h)
+{
+	size_t nodec = 0;
+	const list_t *curr = h;
+
+	while (curr != NULL)
+	{
+		if (curr->str == NULL)
+		{
+			printf("[0] (nil)\n");
+		}
+		else
+		{
+			printf("[%u]%s\n", curr->len, curr->str);
+		}
+		nodec++;
+		curr = curr->next;
+	}
+	return (nodec);
+}
