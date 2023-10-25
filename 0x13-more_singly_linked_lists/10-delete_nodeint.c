53946c858f2755e9b07cb8a08fb1b7eade527a61
Black-ceaser <calvin.annan@gmail.com>
2023-10-25 11:44:49 -0700

    made afew betty changes

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -10,7 +10,7 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 {
 	listint_t *current = *head;
 	listint_t *previous = NULL;
-        unsigned int i = 0;
+	unsigned int i = 0;
 
 	if (*head == NULL)
 	{
