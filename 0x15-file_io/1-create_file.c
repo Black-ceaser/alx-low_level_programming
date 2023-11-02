5df2454f35c62f0e86540115f2087a37d53f91be
Black-ceaser <calvin.annan@gmail.com>
2023-11-02 04:36:02 -0700

    made a few changes

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -20,10 +20,11 @@ int create_file(const char *filename, char *text_content)
 
 	if (fd == -1)
 		return (-1);
-	if (!text_content)
+	if (text_content == NULL)
 		text_content = "";
 	for (nlet = 0; text_content[nlet]; nlet++)
-		rwr = write(fd, text_content, nlet);
+		;
+	rwr = write(fd, text_content, nlet);
 	if (rwr == -1)
 		return (-1);
 	close(fd);
