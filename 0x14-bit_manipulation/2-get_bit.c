fc74c41f1784c09ae64fe3f5051da433610ff03b
Black-ceaser <calvin.annan@gmail.com>
2023-11-24 13:21:52 -0800

    corrected the 2-get_bit.c problem

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -10,7 +10,7 @@ int get_bit(unsigned long int n, unsigned int index)
 {
 	unsigned long int divisor, check;
 
-	if (index > (sizeof(unsigned long intO * 8 - 1))
+	if (index > (sizeof(unsigned long int) * 8 - 1))
 			return (-1);
 		divisor = 1 << index;
 		check = n & divisor;
