ec354c35c6b5cf787ce5708223b64e4d78f7a197
Black-ceaser <calvin.annan@gmail.com>
2023-11-28 04:51:22 -0800

    fixed a problem in 0-read_textfile

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -14,16 +14,16 @@ ssize_t read_textfile(const char *filename, size_t letters)
 
 	if (!filename)
 		return (0);
-	
+
 	fd = open(filename, O_RDONLY);
 
 	if (fd == -1)
 		return (0);
-	
+
 	buff = malloc(sizeof(char) * letters);
 	if (buff == NULL)
 		return (0);
-	
+
 	rd = read(fd, buff, letters);
 	wr = write(STDOUT_FILENO, buff, rd);
 
